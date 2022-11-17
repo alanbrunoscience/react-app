83dd67cd2c93a9a69d95a0f0ed9d2e0e9b86006d
Alan Bruno <alanbrunomath@gmail.com>
2022-11-16 21:45:14 -0300

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryURL: "https://github.com/alanbrunoscience/react-app",
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github']
-}
+  branches: "master",
+  repositoryURL: "https://github.com/alanbrunoscience/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
